388f57aa50fe29fefa60cbbc0bb80c7bbd05de2e
linsheng.yu@okcoin.com <linsheng.yu@okcoin.com>
2019-03-26 17:32:23 +0800

    add flag p2p.addr_book_strict

--- cmd/tendermint/commands/run_node.go
+++ cmd/tendermint/commands/run_node.go
@@ -38,6 +38,7 @@ func AddNodeFlags(cmd *cobra.Command) {
 	cmd.Flags().Bool("p2p.pex", config.P2P.PexReactor, "Enable/disable Peer-Exchange")
 	cmd.Flags().Bool("p2p.seed_mode", config.P2P.SeedMode, "Enable/disable seed mode")
 	cmd.Flags().String("p2p.private_peer_ids", config.P2P.PrivatePeerIDs, "Comma-delimited private peer IDs")
+	cmd.Flags().Bool("p2p.addr_book_strict", config.P2P.AddrBookStrict, "Set true for strict address routability rules, Set false for private or local networks") // okdex
 
 	// consensus flags
 	cmd.Flags().Bool("consensus.create_empty_blocks", config.Consensus.CreateEmptyBlocks, "Set this to false to only produce blocks when there are txs or when the AppHash changes")
