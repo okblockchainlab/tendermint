ecf885c6fc9bb182c2088896ae9544c7c0878f48
banishee <gl12138@live.com>
2019-06-06 13:56:41 +0800

    divide new func from *.go to *_okchain.go

--- libs/cli/flags/log_level.go
+++ libs/cli/flags/log_level.go
@@ -84,7 +84,3 @@ func ParseLogLevel(lvl string, logger log.Logger, defaultLogLevelValue string) (
 
 	return log.NewFilter(logger, options...), nil
 }
-
-func UpdateLogLevel(lvl string) error {
-	return log.UpdateLogLevel(lvl)
-}
